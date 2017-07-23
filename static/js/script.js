9108604d60b0c5cd5453427fcc0ea128a4d8be04
pcjl <pcjl@users.noreply.github.com>
2017-07-23 13:07:42 -0400

    Add loading Add snackbar

---
+++ static/js/script.js
@@ -0,0 +1,40 @@
+jQuery(function($) {
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        var form = $('form');
+        var submitButton = $('button[type=submit]');
+        submitButton.button('loading');
+
+        $.ajax({
+            url: form.attr('url'),
+            type: form.attr('method'),
+            data: form.serialize(),
+            error: function(jqXHR) {
+                $('.form-group[name="password"]').addClass("has-error")
+                submitButton.button('reset');
+            },
+            success: function(data, textStatus, jqXHR) {
+                Snackbar.show( {
+                        text: "Post successful!",
+                        showActionButton: true,
+                        actionText: "View Post",
+                        onActionClick: function() {
+                            window.open(data, "_blank");
+                        }
+                    }
+                );
+                
+                submitButton.button('reset');
+            },
+        });
+
+        return false;
+    });
+
+    $('#datetime').datetimepicker();
+
+    $('.btn-default').click(function() {
+        $('input[type=text]').val("");
+    });
+});
